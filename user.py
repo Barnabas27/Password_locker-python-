d7af6cdf3bb30fc236c1cf4a12c23149be444d56
Barnabas27 <bnyambura032@gmail.com>
2020-11-21 01:32:43 +0300

    add class Credentials,save_credential method and instantiates it's test

--- user.py
+++ user.py
@@ -1,6 +1,6 @@
 class User:
     '''
-    Class that generates new instances of user i.e: username and password
+    Class that generates new instances ofCredentials user i.e: username and password
     '''
     
     def __init__(self,username,password):
@@ -10,3 +10,31 @@ class User:
     
         self.username = username
         self.password = password
+
+class Credentials:
+    
+    
+        
+    """
+    Class Credentials generates instances of what will be contained in the class
+    """
+        
+    User_Credentials_list = []
+        
+    def save_credential(self):
+        
+            
+        Credentials.User_Credentials_list.append(self)
+            
+        """
+        save_credential method that saves credential objects into the user_credential_list
+        """
+    def __init__(self,sm_account,username,pass_word):
+        
+        self.sm_account = sm_account
+        self.username = username
+        self.pass_word = pass_word
+        
+        
+
+            
--- user_test.py
+++ user_test.py
@@ -2,6 +2,8 @@ import unittest
 
 from user import User
 
+from user import Credentials
+
 class TestUser(unittest.TestCase):
     def setUp(self):
         
@@ -11,6 +13,21 @@ class TestUser(unittest.TestCase):
         
         self.assertEqual(self.new_user.username,"Barbez")
         self.assertEqual(self.new_user.password,"123")
+        
+        
+class TestCredentials(unittest.TestCase):
+    """
+    Test subclass that defines test cases for the credentials class behaviours
+    """
+    
+    def setUp(self):
+        
+        self.new_credential = Credentials("twitter","@owslemy","456")
+        
+    def test_init(self):
+        self.assertEqual(self.new_credential.sm_account,"twitter")
+        self.assertEqual(self.new_credential.username,"@owslemy")
+        self.assertEqual(self.new_credential.pass_word,"456")
         
         
 if __name__ =='__main__':
