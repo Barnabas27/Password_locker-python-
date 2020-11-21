ac9cf0c5a66d82874f3fe9a10af0db2f82bef27a
Barnabas27 <bnyambura032@gmail.com>
2020-11-21 03:17:07 +0300

    add main function and the log in logic

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3.8
+import random
+
 from user import User
 
 from user import Credentials
@@ -27,4 +29,31 @@ def display_credential():
     this function displays credentials
     """
     return Credentials.display_credentials()
-    
+
+
+def main():
+    while True:
+        print("Let me make your work easy:select these abbreviations to manouver:to creat an acc use 'new': To log in 'lg': To exit'x'")
+        print('\n')
+        abbreviation = input().lower()
+        
+        print('\n')
+        
+        if abbreviation == 'new':
+            print('Create account')
+            account_created = input()
+            print('Create password')
+            password_created = input()
+            print('Please confirm password')
+            password_confirmed = input()
+            while password_confirmed != password_created:
+                print('Your passwords do not match')
+                password_created = input()
+                print('Make sure your passwords match:')
+                password_confirmed = input()
+            else:
+                print(f"You have successfully created {account_created}")
+            
+        
+if __name__ =='__main__':
+    main()
