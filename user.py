fc341e247a16179fd6fa20aab0fac39eb5d2f4c1
Barnabas27 <bnyambura032@gmail.com>
2020-11-23 12:19:24 +0300

    add test to check whehter User_List can save multiple users

--- user.py
+++ user.py
@@ -2,52 +2,61 @@ class User:
     '''
     Class that generates new instances ofCredentials user i.e: username and password
     '''
+    User_List = []
+    
+    def save_user(self):
+        
+        User.User_List.append(self)
     
     def __init__(self,username,password):
+        
         """
         intro of the __init__method to define properties for our objects
         """  
+        
     
         self.username = username
         self.password = password
+        
+    
 
-class Credentials:
+# class Credentials:
     
     
         
-    """
-    Class Credentials generates instances of what will be contained in the class
-    """
+#     """
+#     Class Credentials generates instances of what will be contained in the class
+#     """
         
-    User_Credentials_list = []
+#     User_Credentials_list = []
         
-    def save_credential(self):
+#     def save_credential(self):
         
             
-        Credentials.User_Credentials_list.append(self)
+#         Credentials.User_Credentials_list.append(self)
             
-        """
-        save_credential method that saves credential objects into the user_credential_list
-        """
-    def __init__(self,sm_account,username,pass_word):
+#         """
+#         save_credential method that saves credential objects into the user_credential_list
+#         """
+#     def __init__(self,sm_account,username,pass_word):
         
-        self.sm_account = sm_account
-        self.username = username
-        self.pass_word = pass_word
+#         self.sm_account = sm_account
+#         self.username = username
+#         self.pass_word = pass_word
         
-    def delete_credential(self):
-        """
-        gets to delete a saved credential
-        """
-        Credentials.User_Credentials_list.remove(self)
+#     def delete_credential(self):
+#         """
+#         gets to delete a saved credential
+#         """
+#         Credentials.User_Credentials_list.remove(self)
         
         
-    @classmethod
-    def display_credentials(cls):
-        """
-        this returns the credential list
-        """
-        return cls.User_Credentials_list
+#     @classmethod
+#     def display_credentials(cls):
+#         """
+#         this returns the credential list
+#         """
+#         return cls.User_Credentials_list
         
         
    
--- user_test.py
+++ user_test.py
@@ -2,7 +2,7 @@ import unittest
 
 from user import User
 
-from user import Credentials
+# from user import Credentials
 
 class TestUser(unittest.TestCase):
     def setUp(self):
@@ -14,62 +14,72 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username,"Barbez")
         self.assertEqual(self.new_user.password,"123")
         
+    def test_save_multiple_user(self):
+        '''
+        this test is to check whether one can save multiple user object to the
+        User_List
+        '''
+        self.new_user.save_user()
+        test_user = User("barbez","12345")
+        test_user.save_user()
+        self.assertEqual(len(User.User_List),2)
         
-class TestCredentials(unittest.TestCase):
-    """
-    Test subclass that defines test cases for the credentials class behaviours
-    """
+        
+# class TestCredentials(unittest.TestCase):
+#     """
+#     Test subclass that defines test cases for the credentials class behaviours
+#     """
     
-    def setUp(self):
+#     def setUp(self):
         
-        self.new_credential = Credentials("twitter","@owslemy","456")
+#         self.new_credential = Credentials("twitter","@owslemy","456")
         
-    def test_init(self):
-        self.assertEqual(self.new_credential.sm_account,"twitter")
-        self.assertEqual(self.new_credential.username,"@owslemy")
-        self.assertEqual(self.new_credential.pass_word,"456")
+#     def test_init(self):
+#         self.assertEqual(self.new_credential.sm_account,"twitter")
+#         self.assertEqual(self.new_credential.username,"@owslemy")
+#         self.assertEqual(self.new_credential.pass_word,"456")
         
-    def test_save_credential(self):
+#     def test_save_credential(self):
         
-        self.new_credential.save_credential()
-        self.assertEqual(len(Credentials.User_Credentials_list),1)
+#         self.new_credential.save_credential()
+#         self.assertEqual(len(Credentials.User_Credentials_list),1)
         
-    def tearDown(self):
-        Credentials.User_Credentials_list = []
-        """
-        the clean up after each test is run
-        """
-    def test_save_multiple_credential(self):
+#     def tearDown(self):
+#         Credentials.User_Credentials_list = []
+#         """
+#         the clean up after each test is run
+#         """
+#     def test_save_multiple_credential(self):
         
-        """
-        this is to check whether we can save multiple contacts
-        """
-        self.new_credential.save_credential()
-        test_credential = Credentials("facebook","@Prada","1234") #this is a new credential
-        test_credential.save_credential()
-        self.assertEqual(len(Credentials.User_Credentials_list),2)
+#         """
+#         this is to check whether we can save multiple contacts
+#         """
+#         self.new_credential.save_credential()
+#         test_credential = Credentials("facebook","@Prada","1234") #this is a new credential
+#         test_credential.save_credential()
+#         self.assertEqual(len(Credentials.User_Credentials_list),2)
     
-    def test_delete_credential(self):
+#     def test_delete_credential(self):
         
-        """
-        this is to test whether a credential can be deleted
-        """
-        self.new_credential.save_credential()
-        test_credential = Credentials("facebook","@Prada","1234") #the new credential created
-        test_credential.save_credential()
+#         """
+#         this is to test whether a credential can be deleted
+#         """
+#         self.new_credential.save_credential()
+#         test_credential = Credentials("facebook","@Prada","1234") #the new credential created
+#         test_credential.save_credential()
         
-        self.new_credential.delete_credential() 
-        """
-        this will delete a credential
-        """
-        self.assertEqual(len(Credentials.User_Credentials_list),1)
+#         self.new_credential.delete_credential() 
+#         """
+#         this will delete a credential
+#         """
+#         self.assertEqual(len(Credentials.User_Credentials_list),1)
         
-    def test_display_all_credentials(self):
+#     def test_display_all_credentials(self):
         
-        """
-        shows a list of all saved user credentials
-        """
-        self.assertEqual(Credentials.display_credentials(),Credentials.User_Credentials_list)
+#         """
+#         shows a list of all saved user credentials
+#         """
+#         self.assertEqual(Credentials.display_credentials(),Credentials.User_Credentials_list)
         
 if __name__ =='__main__':
     unittest.main()
